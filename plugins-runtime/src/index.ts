9af6f34b5591ad2553d4e05a7942f6a5c2982fbb
Juanfran <juanfran.ag@gmail.com>
2024-02-06 11:49:10 +0100

    feat: listen changes

--- plugins-runtime/src/index.ts
+++ plugins-runtime/src/index.ts
@@ -1,5 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function initialize(api: any) {
   console.log('plugin context');
   console.log(api);
+
+  api.addListener('plugin-page', 'page', (page: any) => {
+    console.log('Page Changed:', page);
+  });
+
+  api.addListener('plugin file', 'file', (file: any) => {
+    console.log('File Changed:', file);
+  });
 }
