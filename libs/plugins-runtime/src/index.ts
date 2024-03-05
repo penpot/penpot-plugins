f5ceae87b483a0a65e000f7208b3b677adc0dcf3
Juanfran <juanfran.ag@gmail.com>
2024-03-05 08:52:20 +0100

    fix: unused assigment

--- libs/plugins-runtime/src/index.ts
+++ libs/plugins-runtime/src/index.ts
@@ -8,8 +8,6 @@ repairIntrinsics({
   evalTaming: 'unsafeEval',
 });
 
-globalThis.ɵloadPlugin = ɵloadPlugin;
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function initialize(api: any) {
   console.log('plugin context');
