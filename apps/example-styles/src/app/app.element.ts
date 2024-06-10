5335b50a64cf4a4a205fe667c33402fd0ec81d48
Juanfran <juanfran.ag@gmail.com>
2024-06-10 11:50:38 +0200

    feat: improve styles doc page

--- apps/example-styles/src/app/app.element.ts
+++ apps/example-styles/src/app/app.element.ts
@@ -7,6 +7,30 @@ export class AppElement extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = html;
+
+    Array.from(this.querySelectorAll('template')).forEach((el: HTMLElement) => {
+      const pre = document.createElement('pre');
+      const code = document.createElement('code');
+      code.classList.add('language-html');
+      const removeLineIndentation = el.innerHTML.replaceAll(
+        this.getIndentationSize(el.innerHTML),
+        ''
+      );
+
+      code.textContent = removeLineIndentation;
+
+      pre.appendChild(code);
+
+      el.parentNode?.appendChild(pre);
+      el.remove();
+    });
+
+    (window as any).hljs.highlightAll();
+  }
+
+  getIndentationSize(str: string) {
+    const size = str.length - str.trimStart().length;
+    return ' '.repeat(size - 1);
   }
 }
 customElements.define('app-root', AppElement);
