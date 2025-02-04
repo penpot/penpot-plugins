9a3e6e049b435ceab773dde79d175830c40e75f5
Juanfran <juanfran.ag@gmail.com>
2025-02-04 09:00:02 +0100

    feat: transform color & opacity to rgba

--- apps/colors-to-tokens-plugin/src/app/utils/transform-to-token.ts
+++ apps/colors-to-tokens-plugin/src/app/utils/transform-to-token.ts
@@ -1,47 +1,67 @@
 import { LibraryColor } from '@penpot/plugin-types';
 import { TokenStructure } from '../../model';
 
+function transformToRgba({
+  color,
+  opacity,
+}: Required<Pick<LibraryColor, 'color' | 'opacity'>>) {
+  color = color.slice(1);
+
+  const r = parseInt(color.substring(0, 2), 16);
+  const g = parseInt(color.substring(2, 4), 16);
+  const b = parseInt(color.substring(4, 6), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+interface Color extends LibraryColor {
+  color: string;
+}
+
 export function transformToToken(colors: LibraryColor[]) {
   const result: TokenStructure = {};
 
-  colors.forEach((data) => {
-    const currentOpacity = data.opacity ?? 1;
-    const opacity = currentOpacity < 1 ? Math.floor(currentOpacity * 100) : '';
-    const value = `${data.color}${opacity}`;
-    const names: string[] = data.name.split(' ');
+  colors
+    .filter((data): data is Color => !!data.color)
+    .forEach((data) => {
+      const currentOpacity = data.opacity ?? 1;
+      const value = transformToRgba({
+        opacity: currentOpacity,
+        color: data.color,
+      });
+      const names: string[] = data.name.split(' ');
+      const key: string = data.path.replace(' \\/ ', '/').replace(/ /g, '');
 
-    const key: string = data.path.replace(' \\/ ', '/').replace(/ /g, '');
-
-    if (!result[key]) {
-      result[key] = {};
-    }
-
-    const props = [key, ...names];
-    let acc = result;
-
-    props.forEach((prop, index) => {
-      if (!acc[prop]) {
-        acc[prop] = {};
+      if (!result[key]) {
+        result[key] = {};
       }
 
-      if (index === props.length - 1) {
-        let propIndex = 1;
-        const initialProp = prop;
+      const props = [key, ...names];
+      let acc = result;
 
-        while (acc[prop]?.$value) {
-          prop = `${initialProp}${propIndex}`;
-          propIndex++;
+      props.forEach((prop, index) => {
+        if (!acc[prop]) {
+          acc[prop] = {};
         }
 
-        acc[prop] = {
-          $value: value,
-          $type: 'color',
-        };
-      }
+        if (index === props.length - 1) {
+          let propIndex = 1;
+          const initialProp = prop;
 
-      acc = acc[prop] as TokenStructure;
+          while (acc[prop]?.$value) {
+            prop = `${initialProp}${propIndex}`;
+            propIndex++;
+          }
+
+          acc[prop] = {
+            $value: value,
+            $type: 'color',
+          };
+        }
+
+        acc = acc[prop] as TokenStructure;
+      });
     });
-  });
 
   return result;
 }
