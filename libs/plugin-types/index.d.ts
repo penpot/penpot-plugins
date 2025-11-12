40f69fdc28422b1504bb9abb36f259baea889561
alonso.torres <alonso.torres@kaleidos.net>
2025-11-12 12:02:48 +0100

    feat(plugins-runtime): add data method to image data

--- libs/plugin-types/index.d.ts
+++ libs/plugin-types/index.d.ts
@@ -2273,6 +2273,11 @@ export type ImageData = {
    * Defaults to false if omitted.
    */
   keepAspectRatio?: boolean;
+
+  /**
+   * Returns the imaged data as a byte array.
+   */
+  data(): Promise<Uint8Array>;
 };
 
 /**
